f52a53b6cdc3973f0eb4f8fef20608ac2f689b52
Andrea D'Amico <andrea.damico@gmail.com>
2026-03-13 11:35:18 +0100

    feat: reduced step speeds

--- resources/movement_config.gd
+++ resources/movement_config.gd
@@ -3,5 +3,5 @@ extends Resource
 
 @export var cell_size:= 1.0
 @export var smooth_mode:= false
-@export var step_duration:= 0.16
-@export var turn_duration:= 0.12
+@export var step_duration:= 0.08
+@export var turn_duration:= 0.06
